d0f7cd008a2dfb2e572d4c44afef3240a96de0c6
Nicole Housekeeper <nicolehousekeeper@Nicoles-MacBook-Air.local>
2021-06-02 14:30:51 -0400

    Add queries.

--- schema.sql
+++ schema.sql
@@ -49,3 +49,20 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 PRIMARY KEY (emp_no, dept_no)
 );
 SELECT * FROM departments;
+SELECT * FROM departments;
+SELECT * FROM dept_manager;
+SELECT * FROM dept_employees;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+
+DROP TABLE titles CASCADE;
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+PRIMARY KEY (emp_no, from_date)
+);
